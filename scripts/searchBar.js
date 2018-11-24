2f77a4ffe245f00f453d5d5f521ebfa36f4c575a
elvisplaza <elvis.plaza@hotmail.com>
2018-11-24 14:36:00 -0500

    added search bar

--- scripts/searchBar.js
+++ scripts/searchBar.js
@@ -1,15 +1,37 @@
 
-app.searchBar = function(wordToMatch, country){
+// this will search for the countryes based on the letters they type with the array input
 
-    return  this.country.filter(place =>{
+app.searchBar = (wordToMatch, country) => {
+    return country.filter(function (place) {
         // here we need to figure out if the city or state matches what was searched
         const regex = new RegExp(wordToMatch, 'gi');
-        return place.name.match(regex) || place.capital.match(regex);
+        return place.name.match(regex) || place.capital.match(regex)
     });
 }
 
-app.displayMatches = function(){
-    console.log(this.value)
+// the function that takes the value from the input and maps throught and displays the found information. 
+app.countrySearch = function () {
+    $('#country-input').on('click change keyup copy paste cut', function () {
+        const currentVal = $(this).val();
+        const matchArray = app.searchBar(currentVal, app.countryDataArray);
+
+        const getData = new Promise(function (resolve, reject) {
+            const html = matchArray.map(function (place) {
+                return `<li class="country-list__item"><p>${place.name} - ${place.capital} </p></li>`;
+            });
+            resolve(html);
+        })
+        //determines if the li list will be empty or not. 
+        getData.then(function (array) {
+            console.log("now I am placing data")
+            if ($('#country-input').val() !== "") {
+                $('#country-list').empty().append(array.slice(0, 5));
+                $('#country-list').removeClass("no-display");
+            } else {
+                $('#country-list').addClass("no-display");
+            }
+        })
+    });
 }
 
-const searchInput = document
+
